303fdacc6654b719a0fc47b55912d0f96bb864d2
juangod1 <juan.godfrid@gmail.com>
2017-05-24 12:12:14 -0300

    GameTree

--- src/Model/GameTree.java
+++ src/Model/GameTree.java
@@ -8,14 +8,19 @@ import java.util.ArrayList;
  *  Header structure for the Game Tree
  */
 public class GameTree {
-    private Node root;
+    private Board board;
 
     public GameTree(Board board){
-
+        this.board = board;
     }
 
-    private Board buildTree(Board board){
-        return new Board();//dummy
+    /**
+     * Construye el árbol y retorna el Node representando
+     * la mejor jugada.
+     * @return
+     */
+    private Node buildTree(){
+        return new Node(0,0,false);//dummy
     }
 
     public ArrayList<Node> makeMoves(Board board, boolean isPLayer) {
