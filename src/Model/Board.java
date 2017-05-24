c27d30dba1326bfc1c17f2bc271a89d83ae713ef
juangod1 <juan.godfrid@gmail.com>
2017-05-24 09:56:06 -0300

    Board(Model) Logic

---
+++ src/Model/Board.java
@@ -0,0 +1,71 @@
+package Model;
+
+import Service.Constants;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * The board is represented by a HashSet of player Pieces, a HashSet of AI Pieces,
+ * and a primitive array with:
+ *     0 representing an empty position
+ *     1 representing a position held by the player
+ *     -1 representing a position held by the AI
+ */
+public class Board {
+    private static class Piece{
+        private boolean isPlayer;
+        private Integer xPos;
+        private Integer yPos;
+
+        public Piece(int xPos, int yPos, boolean isPlayer){
+            this.xPos = xPos;
+            this.yPos = yPos;
+            this.isPlayer = isPlayer;
+        }
+
+        public int hashCode() {
+            int result = 17;
+            result = 31 * result + xPos.hashCode();
+            result = 31 * result + yPos.hashCode();
+            return result;
+        }
+    }
+
+    private HashSet<Piece> playerPieces;
+    private HashSet<Piece> aiPieces;
+    private char[][] board = new char[Constants.boardSize][Constants.boardSize];
+
+    public Board(){
+        playerPieces = new ArrayList<>();
+        aiPieces = new ArrayList<>();
+    }
+
+    public boolean addPiece(int xPos, int yPos, boolean isPlayer){
+        if (board[xPos][yPos]!=0)
+            return false;
+        else{
+            if (isPlayer){
+                board[xPos][yPos]=1;
+                playerPieces.add(new Piece(xPos,yPos,true));
+            }
+            else {
+                board[xPos][yPos]=0;
+                aiPieces.add(new Piece(xPos,yPos,false));
+            }
+            return true;
+        }
+    }
+
+    public int playerPiecesCardinal(){
+        return playerPieces.size();
+    }
+
+    public int aiPiecesCardinal(){
+        return aiPieces.size();
+    }
+
+    public boolean gameFinished(){
+        return false;
+    }
+}
---
+++ src/Model/Node.java
@@ -0,0 +1,7 @@
+package Model;
+
+/**
+ * Created by juan on 24/05/17.
+ */
+public class Node {
+}
