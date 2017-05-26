17b61b6026900532c776662f2c87a0419c87de0f
acalatayud <acalatayud@itba.edu.ar>
2017-05-26 16:46:22 -0300

    ReadBoard from txt

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,5 +1,9 @@
 package Controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import Model.Board;
 
 /**
@@ -8,5 +12,33 @@ import Model.Board;
 public class Controller {
     public static Board waitForPlayerMove(Board board){
         return new Board();//dummy para q compile
+    }
+
+    public static Board readBoard(File file){
+      Board board = new Board();
+    	int c;
+    	int xPos = 0;
+    	int yPos = 0;
+
+    	try {
+    		FileInputStream fileIn = new FileInputStream(file);
+    		while ((c = fileIn.read()) != -1) {
+    			if(c == '\n'){
+    				yPos++;
+    				xPos = 0;
+    			}
+    			else {
+    				if(c != ' ')
+    					board.addPiece(xPos, yPos, c=='1');
+    				xPos++;
+    			}
+    		}
+				fileIn.close();
+
+    	} catch (IOException i) {
+    		board = null;
+    	}
+
+    	return board;
     }
 }
