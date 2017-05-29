5ab3c2edfe33548a5d199a009e7f023eca43526c
fdelgado <fdelgado@itba.edu.ar>
2017-05-29 00:59:59 -0300

    Boardview WIP

--- src/View/BoardView.java
+++ src/View/BoardView.java
@@ -11,6 +11,21 @@ import java.awt.*;
  * Created by juan on 23/05/17.
  */
 public class BoardView {
+
+    private class ButtonListener{
+        public void actionPerformed(ActionEvent e){
+            int i = 0;
+            Component[] comp = stoneButtonsPanel.getComponents();
+            while(e.getSource()!= (JButton)comp[i]) {
+                i++;
+            }
+            if(e.getSource() ==(JButton)comp[i]){
+                JButton b = (JButton)comp[i];
+                setPostition(i);
+            }
+        }
+    }
+
     public BoardView() {
         initialize();
     }
@@ -18,19 +33,55 @@ public class BoardView {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel buttonPanel = new JPanel();
+        JPanel StoneButtonsPanel = new JPanel();
         JPanel containerPanel = new JPanel();
 
-        buttonPanel.setLayout(new GridLayout(Constants.boardSize, Constants.boardSize));
+        StoneButtonsPanel.setLayout(new GridLayout(Constants.boardSize, Constants.boardSize));
         for (int i = 0; i< Constants.boardSize* Constants.boardSize; i++) {
-            buttonPanel.add(new JButton("" + (i+1)));
+            JButton b = new JButton("" + (i));
+            ButtonListener bl = new ButtonListener();
+            b.addActionListener(bl);
+            b.setOpaque(false);
+            b.setContentAreaFilled(false);
+            b.setBorderPainted(false);
+            StoneButtonsPanel.add(b);
+
         }
 
-        buttonPanel.setPreferredSize(new Dimension(800, 800));
-        containerPanel.add(buttonPanel);
+        StoneButtonsPanel.setPreferredSize(new Dimension(800, 800));
+        containerPanel.add(StoneButtonsPanel);
 
         frame.getContentPane().add(containerPanel);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void buttonClick(){}
+/**
+ *
+ * */
+    public boolean setPostition(int color, int pos){
+        JButton b;
+        Component[] comp = stoneButtonsPanel.getComponents();
+        if(comp[pos] instanceof JButton ){
+            b = (JButton)comp[pos];
+            if(color ==0){
+                b.setOpaque(false);
+                b.setContentAreaFilled(false);
+            }
+            else if(color == 1) {
+                b.setOpaque(true);
+                b.setContentAreaFilled(true);
+                b.setBackground(Color.black);
+            }
+            else if(color == 2) {
+                b.setOpaque(true);
+                b.setContentAreaFilled(true);
+                b.setBackground(Color.white);
+            }
+            else throw new IllegalParameterException("color must be 0(empty cell), 1(black) or 2(white)");
+            return true;
+        }
+        return false;
     }
 }
